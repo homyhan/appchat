46e015b0de0dcbbcad83c2ac207622ed9ba400d7
homyhan <homyhan0616@gmail.com>
2023-05-25 21:53:26 +0700

    fetch list user friend

--- src/features/Auth/Singin.jsx
+++ src/features/Auth/Singin.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Signin.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { fetchListUser } from "../Chat/thunk";
 
-const Singin = () => {
-    const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const Singin = () => {  
+  const [isLogged, setIsLogged] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const navigate = useNavigate();  
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -22,20 +24,18 @@ const Singin = () => {
 
     // Gửi yêu cầu đăng ký đến API
     const requestData = {
-      action: 'onchat',
+      action: "onchat",
       data: {
-        event: 'REGISTER',
+        event: "REGISTER",
         data: {
           user: username,
-          pass: password
-        }
-      }
+          pass: password,
+        },
+      },
     };
 
-    
-
     // Gửi yêu cầu sử dụng WebSocket
-    const socket = new WebSocket('ws://140.238.54.136:8080/chat/chat');
+    const socket = new WebSocket("ws://140.238.54.136:8080/chat/chat");
     socket.onopen = () => {
       socket.send(JSON.stringify(requestData));
     };
@@ -53,55 +53,57 @@ const Singin = () => {
 
     // Gửi yêu cầu đăng nhập đến API
     const requestData = {
-      action: 'onchat',
+      action: "onchat",
       data: {
-        event: 'LOGIN',
+        event: "LOGIN",
         data: {
           user: username,
-          pass: password
-        }
-      }
+          pass: password,
+        },
+      },
     };
 
     // Gửi yêu cầu sử dụng WebSocket
-    const socket = new WebSocket('ws://140.238.54.136:8080/chat/chat');
+    const socket = new WebSocket("ws://140.238.54.136:8080/chat/chat");
     socket.onopen = () => {
-      socket.send(JSON.stringify(requestData));
+      socket.send(JSON.stringify(requestData));      
     };
 
     // Xử lý phản hồi từ API
-    socket.onmessage = (event) => {
-      const response = JSON.parse(event.data);      
-    if(response.status==="success"){
-      localStorage.setItem("DATA_RELOGIN", JSON.stringify(response.data));        
-      localStorage.setItem("USERNAME", JSON.stringify(username));        
-      dispatch({
-        type:"LOGIN",
-        payload: {
-          code: response.data,
-          username
-        }
-      })
-      return navigate("/chat");
-      
-    }else{
+    socket.onmessage = async (event) => {
+      const response = JSON.parse(event.data);
+
+      console.log(response);
+      if (response.status === "success") {
+        localStorage.setItem("DATA_RELOGIN", JSON.stringify(response.data));
+        localStorage.setItem("USERNAME", JSON.stringify(username));
+        console.log("socket khi dn thanh cong", socket);
+
+       await dispatch({
+          type: "LOGIN",
+          payload: {
+            code: response.data,
+            username,
+            socketObj: socket
+          },
+        });
+        
+        // Kiểm tra nếu chưa gọi yêu cầu "GET_USER_LIST" sau khi đăng nhập thành công
+        if (!isLogged) {
+          setIsLogged(true);
+          await dispatch(fetchListUser(socket));
+          
+        }    
+
+        return navigate("/chat");
+      } else {
         console.log("login error");
-    }
+      }     
 
-    // if (response.event === 'AUTH' && response.status === 'error') {
-    //   console.log('Đăng nhập không thành công: ' + response.mes);
-    //   // Hiển thị thông báo lỗi cho người dùng
-    //   alert('Đăng nhập không thành công: ' + response.mes);
-    // } else if (response.success) {
-    //   console.log('Đăng nhập thành công');
-    //   // Thực hiện các hành động sau khi đăng nhập thành công
-    // } else {
-    //   console.log('Đăng nhập không thành công');
-    // }
-
-      socket.close();
+      // socket.close();
     };
   };
+ 
 
   return (
     <div className="main">
@@ -111,9 +113,18 @@ const Singin = () => {
           <label htmlFor="chk" aria-hidden="true">
             Sign up
           </label>
-          <input type="text" value={username} onChange={handleUsernameChange}  required />
-          
-          <input type="password" value={password} onChange={handlePasswordChange} />
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
           <button type="submit">Sign up</button>
         </form>
       </div>
@@ -122,8 +133,18 @@ const Singin = () => {
           <label htmlFor="chk" aria-hidden="true">
             Login
           </label>
-          <input type="text" value={username} onChange={handleUsernameChange} required />
-          <input type="password" value={password} onChange={handlePasswordChange} required />
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
           <button type="submit">Login</button>
         </form>
       </div>
--- src/features/Chat/ListUser.jsx
+++ src/features/Chat/ListUser.jsx
@@ -1,148 +1,70 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./ChatMain.css";
+import CreateRoom from "./CreateRoom";
+import { FindPerson } from "./FindPerson";
+import { fetchMesPeople, fetchMesRoom } from "./thunk";
 
 const ListUser = () => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    // Gửi yêu cầu lấy danh sách người dùng đến API
-    const requestData = {
-      action: "onchat",
-      data: {
-        event: "GET_USER_LIST",
+  const dispatch = useDispatch();
+  const { listUser } = useSelector((state) => state.chat);
+  const { socket, user } = useSelector((state) => state.auth);
+  const handleChat = async (username, type) => {
+    console.log(username === "");
+    await dispatch({
+      type: "TO_USER1",
+      payload: {
+        nameChatUser: username,
+        isRoom: type,
       },
-    };
+    });
+    if (type === 1) {
+      await dispatch(fetchMesRoom(socket, username));
+    } else {
+      await dispatch(fetchMesPeople(socket, username));
+    }
 
-    // Gửi yêu cầu sử dụng WebSocket
-    const socket = new WebSocket("ws://140.238.54.136:8080/chat/chat");
-    socket.onopen = () => {
-      socket.send(JSON.stringify(requestData));
-    };
-
-    // Xử lý phản hồi từ API
-    socket.onmessage = (event) => {
-      socket.close();
-    };
-  }, []);
+    console.log(username, type);
+  };
 
   return (
-    
-    <section className="discussions">
-      <div className="discussion search">
-        <div className="searchbar">
-          <i className="fa fa-search" aria-hidden="true" />
-          <input type="text" placeholder="Search..." />
-        </div>
+    <section style={{ paddingRight: "0px" }} className="discussions">
+      <div style={{ display: "flex" }} className="discussion search">
+        <CreateRoom></CreateRoom>
+        <FindPerson></FindPerson>
       </div>
       <div className="listUserChat">
-        <div className="discussion message-active">
-          <div
-            className="photo"
-            style={{
-              backgroundImage:
-                "url(https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80)",
-            }}
-          >
-            <div className="online" />
-          </div>
-          <div className="desc-contact">
-            <p className="name">Nguyễn Đình Lưu</p>
-            <p className="message">9 pm at the bar if possible 😳</p>
-          </div>
-          <div className="timer">12 sec</div>
-        </div>
-        <div className="discussion">
-          <div
-            className="photo"
-            style={{
-              backgroundImage:
-                "url(http://e0.365dm.com/16/08/16-9/20/theirry-henry-sky-sports-pundit_3766131.jpg?20161212144602)",
-            }}
-          >
-            <div className="online" />
-          </div>
-          <div className="desc-contact">
-            <p className="name">Dave Corlew</p>
-            <p className="message">
-              Let's meet for a coffee or something today ?
-            </p>
-          </div>
-          <div className="timer">3 min</div>
-        </div>
-        <div className="discussion">
-          <div
-            className="photo"
-            style={{
-              backgroundImage:
-                "url(https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80)",
-            }}
-          ></div>
-          <div className="desc-contact">
-            <p className="name">Jerome Seiber</p>
-            <p className="message">I've sent you the annual report</p>
-          </div>
-          <div className="timer">42 min</div>
-        </div>
-        <div className="discussion">
-          <div
-            className="photo"
-            style={{
-              backgroundImage:
-                "url(http://thomasdaubenton.xyz/portfolio/images/photo.jpg)",
-            }}
-          >
-            <div className="online" />
-          </div>
-          <div className="desc-contact">
-            <p className="name">Thomas Dbtn</p>
-            <p className="message">See you tomorrow ! 🙂</p>
-          </div>
-          <div className="timer">2 hour</div>
-        </div>
-        <div className="discussion">
-          <div
-            className="photo"
-            style={{
-              backgroundImage:
-                "url(https://images.unsplash.com/photo-1553514029-1318c9127859?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80)",
-            }}
-          ></div>
-          <div className="desc-contact">
-            <p className="name">Elsie Amador</p>
-            <p className="message">What the f**k is going on ?</p>
-          </div>
-          <div className="timer">1 day</div>
-        </div>
-        <div className="discussion">
-          <div
-            className="photo"
-            style={{
-              backgroundImage:
-                "url(https://images.unsplash.com/photo-1541747157478-3222166cf342?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80)",
-            }}
-          ></div>
-          <div className="desc-contact">
-            <p className="name">Billy Southard</p>
-            <p className="message">Ahahah 😂</p>
-          </div>
-          <div className="timer">4 days</div>
-        </div>
-        <div className="discussion">
-          <div
-            className="photo"
-            style={{
-              backgroundImage:
-                "url(https://images.unsplash.com/photo-1435348773030-a1d74f568bc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80)",
-            }}
-          >
-            <div className="online" />
-          </div>
-          <div className="desc-contact">
-            <p className="name">Paul Walker</p>
-            <p className="message">You can't see me</p>
-          </div>
-          <div className="timer">1 week</div>
-        </div>
+        {listUser?.map((item, index) => {
+          return (
+            <div
+              key={index}
+              style={
+                item.name === user.username || item.name === ""
+                  ? { display: "none" }
+                  : {}
+              }
+              className="discussion"
+              onClick={() => handleChat(item.name, item.type)}
+            >
+              <div
+                className="photo"
+                style={{
+                  backgroundImage:
+                    "url(http://e0.365dm.com/16/08/16-9/20/theirry-henry-sky-sports-pundit_3766131.jpg?20161212144602)",
+                }}
+              >
+                <div className="online" />
+              </div>
+              <div className="desc-contact">
+                <p className="name">{item.name}</p>
+                <p className="message">
+                  Let's meet for a coffee or something today ?
+                </p>
+              </div>
+              <div className="timer">3 min</div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
